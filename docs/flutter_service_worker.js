fa674553797fb908f802e04bf2abd1cf05d0a107
Кадров Марсель Робертович <mkadrov@corp.finam.ru>
2023-06-03 23:22:41 +0500

    hotfix

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b77924c9274556a45f26d6ed036e3073",
-"index.html": "6c0b1fd3866914591b97f56aa3965e98",
-"/": "6c0b1fd3866914591b97f56aa3965e98",
+"index.html": "694ef30c98ddf305ba781ce47e0045af",
+"/": "694ef30c98ddf305ba781ce47e0045af",
 "main.dart.js": "b17a2ccf56a748f1fcc358df99823e91",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
